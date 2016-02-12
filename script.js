1ac2bfc378362d1d150500157f0b90457ba9fca5
Keakone <keanucarnevale@gmail.com>
2016-02-12 21:18:49 +1300

    added non-essential styling

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 
-  // getting html elements
-// var numPlayers = 2;
-var trackLength = 11;
+// var trackLength = document.getElementById("chooseTrackLength").value; 
+var trackLength = 10;
 var strip1 = document.querySelector("#player1_strip");
 var strip2 = document.querySelector("#player2_strip");
-//var playerOneStripSelector = "#player1_strip";
-//var playerTwoStripSelector = "#player2_strip";
 var p1score = 0;
 var p2score = 0;
 
@@ -45,21 +42,13 @@ document.addEventListener("keyup", keyPressed, false);
 		movingCar = movingCar.nextElementSibling;
 		if (movingCar == null) {
 			alert( "Woohoo! Player"+playerNumber+" wins!");
+			location.reload()
 		};
 		movingCar.classList.add("active");
-
-		// movingCar.nextElementSibling = movingCar.classList.add('.active');
 		};
-		// movingCar = movingCar.nextElementSibling;
-		// movingCar.classList.add('.active');
-		// movingCar.previousElementSibling.classList.remove('.active');
+
 	
 //appending & taking away the active class, giving the illusion of movement??//
-
-
-
-
-//write a winner function, could be a for loop that checks against the tracklength??
 
 
 
