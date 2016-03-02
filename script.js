817fc48fc35d14060b987398ece4833ce534ecb2
Keakone <keanucarnevale@gmail.com>
2016-03-02 16:15:29 +1300

    read thrugh old code, wrote statement to recognise when you hit an obstacle

--- script.js
+++ script.js
@@ -36,19 +36,21 @@ document.addEventListener("keyup", keyPressed, false);
 
 //moveCar needs to find which player strip to initiate movement
 	var moveCar = function (playerNumber) {
-		console.log("#player"+ playerNumber +"_strip .active")
+		// console.log("#player"+ playerNumber +"_strip .active")
 		var movingCar = document.querySelector("#player"+ playerNumber +"_strip .active");
-		var obstacle = document.querySelector(".obstacle");
+		var obstacle = document.querySelector("#player"+ playerNumber +"_strip .obstacle");
+
 		movingCar.classList.remove("active");
 		movingCar = movingCar.nextElementSibling;
 		if (movingCar == null) {
 			alert( "Woohoo! Player"+playerNumber+" wins!");
 			location.reload()
+		} else if (movingCar == obstacle) {
+			console.log(playerNumber);
 		} else {
 		};
 		movingCar.classList.add("active");
 		};
-
 	
 //create an obstacle to jump over
 //a td element has the class of obstacle, if any car touches it the game is over for that player
@@ -56,6 +58,8 @@ document.addEventListener("keyup", keyPressed, false);
 //if you dont press spacebar and movingcar gets to the class of obstacle you lose
 //if movingcar.nextelement sibling is .obstacle and you press q/p, you lose, spacebar must be pressed to pass
 
+	var hitObstacle = function (playerNumber) {
 
+	}
 
 
